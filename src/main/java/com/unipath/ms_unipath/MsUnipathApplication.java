950950f58bbc8ae796f2305738386025dee27a56
AndreeCarrillo <u20211b589@upc.edu.pe>
2025-06-08 18:11:58 -0500

    initial configuration

---
+++ src/main/java/com/unipath/ms_unipath/MsUnipathApplication.java
@@ -0,0 +1,13 @@
+package com.unipath.ms_unipath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUnipathApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsUnipathApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unipath/ms_unipath/MsUnipathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unipath.ms_unipath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUnipathApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
